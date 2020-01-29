d84d86609abc8d614ee9535653b0874689b53ba5
theodore-lun <53496663+theodore-lun@users.noreply.github.com>
2020-01-29 15:23:52 -0700

    Update deletionScript.py

--- VariantPrograms/deletionScript.py
+++ VariantPrograms/deletionScript.py
@@ -7,8 +7,3 @@ variantName = input ('What is variant ID?')
 
 with open(variantName + '.txt', 'x') as variantFile:
     variantFile.write(variant)
-
-#range=chr7:117480011-117627910
-#>hg38_knownGene_ENST00000648260.1 range=chr7:117480011-117627910 5'pad=0 3'pad=0 strand=+ repeatMasking=none
-#ENST00000648260.1
-#genomicedittest.txt
